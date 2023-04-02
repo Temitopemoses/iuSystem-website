3ee0b33f563ee8ecba28c7b636b898eaf01e2aae
Temitope Moses <101447242+Temitopemoses@users.noreply.github.com>
2023-04-02 07:18:47 +0100

    Update my_script.js
    commit

--- my_script.js
+++ my_script.js
@@ -1,8 +1,8 @@
-// Get the button and the navbar elements
-var button = document.getElementById("menu-toggle");
-var navbar = document.getElementById("navbar");
+const menuToggle = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
 
-// Add an event listener to the button to toggle the navbar visibility when clicked
-button.addEventListener("click", function() {
-    navbar.classList.toggle("hidden");
+menuToggle.addEventListener("click", () => {
+  const isOpen = menuToggle.getAttribute("aria-expanded") === "true";
+  menuToggle.setAttribute("aria-expanded", !isOpen);
+  mobileMenu.classList.toggle("hidden");
 });
